652282ce71d5eb45369cd48bfbd36549d79d9365
Pauk456 <456pauk@mail.ru>
2024-02-07 00:21:01 +0700

    Изменил интерфейс на виртуальную класс

--- Solving_Linear_Equations/include/Solving_Linear_Equations.h
+++ Solving_Linear_Equations/include/Solving_Linear_Equations.h
@@ -4,36 +4,35 @@
 #include<cmath>
 #include"Matrix.h"
 
-class Solving_Linear_Equations_interface
+class Solving_Linear_Equations_virtual
 {
 private:
-	virtual double multiply_row_by_column(const std::vector<double>& row, const std::vector<double>& column) const = 0;
-	virtual double find_norm(const std::vector<double>& row) const = 0;
 	virtual void proximity_function() = 0;
 	virtual bool accuracy_check(double epsilon) const = 0;
 public:
 	static const double ti_plus;
 	static const double ti_minus;
-
-	virtual std::vector<double> execute(double epsilon) = 0;
-	virtual ~Solving_Linear_Equations_interface() = default;
-};
-
-
-class Solving_Linear_Equations_usual : Solving_Linear_Equations_interface
-{
-private:
 	int N;
 	Matrix A;
 	std::vector<double> b;
 	std::vector<double> x;
 	double norm_denominator;
+
+	Solving_Linear_Equations_virtual(Matrix A, std::vector<double> x, std::vector<double> b);
+	double multiply_row_by_column(const std::vector<double>& row, const std::vector<double>& column) const;
+	double find_norm(const std::vector<double>& row) const;
+	virtual std::vector<double> execute(double epsilon) = 0;
+	virtual ~Solving_Linear_Equations_virtual() = default;
+};
+
+
+class Solving_Linear_Equations_usual : Solving_Linear_Equations_virtual
+{
 private:
-	double multiply_row_by_column(const std::vector<double>& row, const std::vector<double>& column) const override;
-	double find_norm(const std::vector<double>& row) const override;
 	void proximity_function() override;
 	bool accuracy_check(double epsilon) const override;
 public:
-	Solving_Linear_Equations_usual(Matrix A, std::vector<double> x, std::vector<double> b);
+	Solving_Linear_Equations_usual(Matrix A, std::vector<double> x, std::vector<double> b) 
+		: Solving_Linear_Equations_virtual(A, x, b) {};
 	virtual std::vector<double> execute(double epsilon) override;
 };
