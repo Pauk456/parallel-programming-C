f0b60cc26e629e0b6b50b38ff22cac129396d507
Vova <456pauK@mail.ru>
2024-03-27 12:11:57 +0700

    s

--- Matrix_parallel/Matrix_multiplication.c
+++ Matrix_parallel/Matrix_multiplication.c
@@ -56,13 +56,44 @@ void Matrix_initialization(Matrix* Matrix)
 	}
 }
 
-void forward_step1(MPI_Comm commGrib)
+void grib_initialization(MPI_Comm commGrib, MPI_Datatype row_type, MPI_Datatype col_type, Matrix matrix1, Matrix matrix2)
 {
+	int dims[2] = { 0,0 }, coords[2], reorder = 1;
+	int size, rank, sizey, sizex, ranky, rankx;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Dims_create(size, 2, dims);
+	sizey = dims[0]; sizex = dims[1];
 	
+	int periods[2] = { 0,0 }, reorder = 1;
+
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &commGrib);
+	
+	MPI_Type_contiguous(matrix1.rows, MPI_DOUBLE, &row_type);
+	MPI_Type_vector(matrix2.cols, 1, matrix2.rows, MPI_DOUBLE, &col_type);
+	MPI_Type_commit(&row_type);
+	MPI_Type_commit(&col_type);
 }
 
-void forward_everyone(MPI_Comm commGrib)
+void colm_row_initialization(MPI_Comm commGrib, MPI_Comm commCol, MPI_Comm commRow)
 {
+	int remains_dims[2];
+	remains_dims[0] = true;
+	remains_dims[1] = false;
+	MPI_Cart_sub(commGrib, remains_dims, commColm); //  Создание столбцов
+	MPI_Scatterv(); // Рассылает?
+}
+
+void forward_step1(MPI_Comm commGrib, int size, int rank)
+{
+
+}
+
+void forward_everyone(MPI_Comm commGrib, MPI_Datatype row_type, MPI_Datatype col_type)
+{
+	int size, rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	forward_step1(commGrib, rank, size);
 }
 
 int main(int argc, char* argv[])
@@ -75,22 +106,6 @@ int main(int argc, char* argv[])
 	N2 = N3 = 100;
 	N4 = 100;
 	
-	int dims[2] = { 0,0 }, coords[2], reorder = 1;
-	int size, rank, sizey, sizex, ranky, rankx;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Dims_create(size, 2, dims);
-	sizey = dims[0]; sizex = dims[1];
-	MPI_Comm commGrib;
-	int periods[2] = { 0,0 }, reorder = 1;
-	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &commGrib);
-
-	MPI_Datatype row_type, col_type;
-	MPI_Type_contiguous(N1, MPI_DOUBLE, &row_type);
-	MPI_Type_vector(N, 1, N, MPI_DOUBLE, &col_type);
-	MPI_Type_commit(&row_type);
-	MPI_Type_commit(&col_type);
-
-
 	Matrix Matrix1;
 	Matrix1.rows = N1;
 	Matrix1.cols = N2;
@@ -99,6 +114,11 @@ int main(int argc, char* argv[])
 		Matrix_create(&Matrix1);
 		Matrix_initialization(&Matrix1);
 	}
+
+	MPI_Comm commGrib;
+	MPI_Datatype row_type, col_type;
+	grib_initialization(commGrib, rowType, colType, matrix1, matrix2);
+	forward_everyone(commGrib, row_type, col_type);
 	
 
 
